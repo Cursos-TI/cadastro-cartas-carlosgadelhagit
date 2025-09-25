fa06126f86bcafccd7ad98a7512f4577ef9175e6
carlosgadelhagit <carloshenriquegadelha026@gmail.com>
2025-09-25 03:04:27 +0000

    Novo commit

---
+++ .vscode/CartasSuperTrunfo2.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+// Carta numero: 2
+// Estado: Ceara
+// Codigo da carta: A02
+// cidade: Santa quiteria 
+int main() {
+  
+  int Populaçao;
+  long Pib, Area;
+  int Pontos;
+
+  printf("Digite a populaçao da cidade: \n");
+  scanf("%d", &Populaçao);
+
+  printf("Digite o pib da cidade: \n");
+  scanf("%ld", &Pib);
+
+  printf("Digite a area da cidade: \n");
+  scanf("%ld",&Area);
+
+  printf("Digite o ponto turistico da cidade: \n");
+  scanf("%d", &Pontos);
+
+  printf("Numero da populaçao: %d \n, Populaçao");
+  printf("Digite o pib da cidade: %ld \n, Pib");
+  printf("Digite a area da cidade: %ld \n, Area");
+  printf("Digite o ponto turistico: %d \n, Ponto");
+
+
+
+return 0;
+} 
---
+++ .vscode/CartasSuperTrunfo3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+// Carta numero: 3
+// Estado: Ceara
+// Codigo da carta: A03
+// cidade: Massape
+
+int main() {
+  
+  int Populaçao;
+  long Pib, Area;
+  int Pontos;
+
+  printf("Digite a populaçao da cidade: \n");
+  scanf("%d", &Populaçao);
+
+  printf("Digite o pib da cidade: \n");
+  scanf("%ld", &Pib);
+
+  printf("Digite a area da cidade: \n");
+  scanf("%ld",&Area);
+
+  printf("Digite o ponto turistico da cidade: \n");
+  scanf("%d", &Pontos);
+
+  printf("Numero da populaçao: %d \n, Populaçao");
+  printf("Digite o pib da cidade: %ld \n, Pib");
+  printf("Digite a area da cidade: %ld \n, Area");
+  printf("Digite o ponto turistico: %d \n, Ponto");
+
+
+
+return 0;
+} 
